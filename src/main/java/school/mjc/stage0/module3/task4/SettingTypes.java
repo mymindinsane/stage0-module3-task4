f778cf9a5050e9fd9c5df1d60f0309c8de7599d6
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2024-02-29 17:11:31 +0300

    Update SettingTypes.java

--- src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
+++ src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
@@ -4,7 +4,7 @@ public class SettingTypes {
     public static void main(String[] args) {
        long first = 10_000_000_000L;
        char second = 'a';
-       bool third = 5 > 9;
+       boolean third = 5 > 9;
        double forth = 12.90434323324342;
        float fifth = 12.3F;
        byte sixth = 1;
